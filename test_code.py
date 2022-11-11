1588471ca7b2d921e861aad6d1bed8fc74562d48
Thomas Snellgrove <tsnellgrove@hotmail.com>
2022-11-11 11:46:39 -0500

    test update for pull test

--- test_code.py
+++ test_code.py
@@ -2,3 +2,7 @@
 # name: Tom Snellgrove
 # date: Nov 11, 2022
 # purpose: practice git-based replication between iPad WorkingCopy / Pythonista and Mac VS Code
+
+# Git repo is now cloned in VS Code - next is pull test
+
+
